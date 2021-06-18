8af6eb23d0a323733f608a12022b02861c039738
michaellouzc <michaellouzc@gmail.com>
2021-06-18 23:09:05 +0800

    ~

--- lubuntu20.sh
+++ lubuntu20.sh
@@ -18,12 +18,16 @@ deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe m
 deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
 EOM
 
-apt update     # 刷新存储库索引
+apt update # 刷新存储库索引
+apt-get purge -y 2048-qt ark bluedevil compton featherpad info k3b kcalc LibreOffice* noblenote partitionmanager qpdfview qtpass quassel screengrab skanlite system-config-printer transmission-qt trojita usb-creator-kde vim vlc
+apt-get autoremove -y
 apt upgrade -y #升级所有可升级的软件包
 
 ###vmware
 # # virtualbox 虚拟机工具，用来实现文件共享、文件拖放和剪贴板共享
-apt install virtualbox-guest-dkms virtualbox-guest-x11
+apt install virtualbox-guest-dkms virtualbox-guest-x11 -y
+
+apt-get clean
 
 # # 自动挂载共享文件夹 #如果没有设置共享文件夹，好像会导致无法开机
 cp /etc/fstab /etc/fstab.bak
